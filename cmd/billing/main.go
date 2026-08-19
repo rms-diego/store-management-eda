655d6750b39f00e2d6e3a29e044ca0e798885e7e
Diego Ramos <diegorbrito9@gmail.com>
2026-08-19 18:34:12 -0300

    chore: create a initial apps
    - order
    
    - inventory
    
    - billing

--- cmd/billing/main.go
+++ cmd/billing/main.go
@@ -3,7 +3,7 @@ package main
 import "github.com/rms-diego/store-management-eda/pkg/app"
 
 func main() {
-	app, err := app.Init("orders")
+	app, err := app.Init("billings")
 	if err != nil {
 		panic(err)
 	}
